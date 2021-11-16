4b35647769ea3ca87221497ca3a940fb7c53f482
AliVik <alvikul1994@gmail.com>
2021-11-16 22:46:39 +0200

    импорт и подключение функции getDataFromAPI

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import './sass/main.scss';
+import getDataFromAPI from './js/api-sources'
+
+const refs= {
+    form: document.querySelector('#search-form'),
+    button: document.querySelector('button[type="submit"]'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    
+    getDataFromAPI(evt);
+}
+
+
+
+
