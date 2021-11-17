f1194a292b1e0e94e87d40f71d01cc31d41f0bda
AliVik <alvikul1994@gmail.com>
2021-11-17 22:31:39 +0200

    создаёт функцию создания разметки

--- src/index.js
+++ src/index.js
@@ -1,18 +1,52 @@
-import getDataFromAPI from './js/api-sources'
+import DataFromAPI from './js/data-from-api'
+
 
 const refs= {
     form: document.querySelector('#search-form'),
     button: document.querySelector('button[type="submit"]'),
+    gallery: document.querySelector('.gallery'),
+    input: document.querySelector('input[name="searchQuery"]'),
 }
+const dataFromApi = new DataFromAPI;
 
 refs.form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(evt) {
     evt.preventDefault();
-    
-    getDataFromAPI(evt);
+    dataFromApi.value = evt.currentTarget.elements.searchQuery.value;
+    console.log(dataFromApi.getDataFromAPI(dataFromApi.value))
+    dataFromApi.getDataFromAPI(dataFromApi.value);
+   
 }
 
+function createCardMarkup(hits) {
+   const markup = hits.map(hit => {
+        return `<a href="${hit.largeImageURL}" class="photo-card">
+  <img src="${hit.webformatURL}" alt="${hit.tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>
+      <span>${hit.likes}</span>
+    </p>
+    <p class="info-item">
+      <b>Views</b>
+      <span>${hit.views}</span>
+    </p>
+    <p class="info-item">
+      <b>Comments</b>
+      <span>${hit.comments}</span>
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>
+      <span>${hit.downloads}</span>
+    </p>
+  </div>
+</a>`
+   })
+    
+    gallery.insertAdjacentHTML('beforeend', markup);
 
+    
+}
 
 
